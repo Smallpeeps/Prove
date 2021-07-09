504408c4638534bc2b2d251f95d3cc9bf0183570
Michele Bonacina <michele.bonacina@gmail.com>
2021-07-09 10:43:42 +0200

    aggiunto elenco attività

--- src/app/attivita-elenco/attivita-elenco.component.ts
+++ src/app/attivita-elenco/attivita-elenco.component.ts
@@ -1,15 +1,15 @@
+import { Attivita } from './../model/attivita';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-attivita-elenco',
   templateUrl: './attivita-elenco.component.html',
-  styleUrls: ['./attivita-elenco.component.scss']
+  styleUrls: ['./attivita-elenco.component.scss'],
 })
 export class AttivitaElencoComponent implements OnInit {
+  elencoAttivita: Attivita[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
