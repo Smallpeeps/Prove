f44fe935f5d110398906bc24372c0aff8187c04c
Michele Bonacina <michele.bonacina@gmail.com>
2021-07-09 11:16:15 +0200

    aggiunto dati di test

--- src/app/attivita-elenco/attivita-elenco.component.ts
+++ src/app/attivita-elenco/attivita-elenco.component.ts
@@ -11,5 +11,25 @@ export class AttivitaElencoComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    // carica l'elenco delle attività
+    // TODO: questo elenco è fittizio, solo per test!!
+    this.elencoAttivita = [
+      {
+        dataOraInizio: new Date(2021, 6, 8, 8, 29, 0),
+        dataOraFine: new Date(2021, 6, 8, 12, 31, 0),
+        descrizione: 'Apprendimento concetti base Angular',
+      },
+      {
+        dataOraInizio: new Date(2021, 6, 8, 14, 2, 0),
+        dataOraFine: new Date(2021, 6, 8, 18, 10, 0),
+        descrizione: 'Applicazione di esempio',
+      },
+      {
+        dataOraInizio: new Date(2021, 6, 9, 8, 32, 0),
+        dataOraFine: new Date(2021, 6, 9, 12, 29, 0),
+        descrizione: 'Approfonfimento concetti base Angular',
+      },
+    ];
+  }
 }
