dca5ef9e21e72d2e0353750079c599f668c660ee
Michele Bonacina <michele.bonacina@gmail.com>
2021-07-12 09:05:55 +0200

    spostato elenco attività nel service

--- src/app/attivita.service.ts
+++ src/app/attivita.service.ts
@@ -1,9 +1,34 @@
+import { Attivita } from './model/attivita';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AttivitaService {
+  elencoAttivita: Attivita[] = [];
 
-  constructor() { }
+  constructor() {
+    // carica l'elenco delle attività
+    // TODO: questo elenco è fittizio, solo per test!!
+    this.elencoAttivita = [
+      {
+        dataOraInizio: new Date(2021, 6, 8, 8, 29, 0),
+        dataOraFine: new Date(2021, 6, 8, 12, 31, 0),
+        descrizione: 'Apprendimento concetti base Angular',
+        referente: 'Michele BONACINA',
+      },
+      {
+        dataOraInizio: new Date(2021, 6, 8, 14, 2, 0),
+        dataOraFine: new Date(2021, 6, 8, 18, 10, 0),
+        descrizione: 'Applicazione di esempio',
+        referente: 'Michele BONACINA',
+      },
+      {
+        dataOraInizio: new Date(2021, 6, 9, 8, 32, 0),
+        dataOraFine: new Date(2021, 6, 9, 12, 29, 0),
+        descrizione: 'Approfondimento concetti base Angular',
+        referente: 'Michele BONACINA',
+      },
+    ];
+  }
 }
